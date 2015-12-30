fe48658ea2a4fc88a644f432635240fa092e92e6
saúl <srttorres@gmail.com>
2015-12-30 18:57:38 +0100

    Da un problema al llamar a init()

--- src/encryption_DESede168/App.java
+++ src/encryption_DESede168/App.java
@@ -1,12 +1,22 @@
 package encryption_DESede168;
 
-public class App {
+import java.security.Key;
 
+import javax.crypto.spec.SecretKeySpec;
+
+public class App {
+    private static final String TIPO = "DESede";
+    private static final byte[] CLAVE = new byte[]{'1','2','3','4','5','6','7','8','9','0','1','2','3'};
 	public static void main(String args[]) throws Exception{
 		String message = "THIS IS SPARTA";
-		String key = "1234";
-		String encryptedMessage = TripleDES168.cifrar(message);		
+		Key key = generateKey();
+
+		String encryptedMessage = TripleDES168.cifrar(message, key);		
 		System.out.println("Mensaje en claro: " + message);
 		System.out.println("Mensaje cifrado: "+ encryptedMessage);
 	}
+    private static Key generateKey() throws Exception {
+        Key key = new SecretKeySpec(CLAVE, TIPO);
+        return key;
+}
 }
